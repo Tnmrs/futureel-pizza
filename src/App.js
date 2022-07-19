092a634c34da1fdfce51ca5d194767a0d34b1fe8
tnmrs <promomyers@gmail.com>
2022-07-20 01:10:32 +0300

    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Sort from './components/Sort';
+import Categories from './components/Categories';
+import PizzaBlock from './components/pizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Mexico" price="10" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
